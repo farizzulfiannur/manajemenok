cf3f6d77380093852449b9dc799a032ceae16df2
farizzulfiannur <farizzulfiannur@gmail.com>
2023-07-10 10:22:36 +0700

    login register tampilan

---
+++ resources/views/register.blade.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Login V12</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="icon" type="image/png" href="images/icons/favicon.ico" />
+
+    <link rel="stylesheet" type="text/css" href="{{ asset('login/vendor/bootstrap/css/bootstrap.min.css') }}">
+
+    <link rel="stylesheet" type="text/css"
+        href="{{ asset('login/fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}">
+
+    <link rel="stylesheet" type="text/css" href="{{ asset('login/fonts/Linearicons-Free-v1.0.0/icon-font.min.css') }}">
+
+    <link rel="stylesheet" type="text/css" href="{{ asset('login/vendor/animate/animate.css') }}">
+
+    <link rel="stylesheet" type="text/css" href="{{ asset('login/vendor/css-hamburgers/hamburgers.min.css') }}">
+
+    <link rel="stylesheet" type="text/css" href="{{ asset('login/vendor/select2/select2.min.css') }}">
+
+    <link rel="stylesheet" type="text/css" href="{{ asset('login/css/util.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('login/css/main.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('login/css/custom.css') }}">
+</head>
+
+<body>
+    <div class="limiter">
+        <div class="container-login100" style="background-image: url('images/img-01.jpg');">
+            <div class="wrap-login100 p-b-30">
+                <form class="login100-form validate-form">
+                    <div class="login100-form-avatar">
+                        <img src="{{ asset('login/images/SiNotes.png') }}" alt="logo">
+                    </div>
+                    <div class="wrap-input100 validate-input m-b-10" data-validate="Username is required">
+                        <input class="input100" type="text" name="username" placeholder="Username">
+                        <span class="focus-input100"></span>
+                        <span class="symbol-input100">
+                            <i class="fa fa-user"></i>
+                        </span>
+                    </div>
+                    <div class="wrap-input100 validate-input m-b-10" data-validate="Password is required">
+                        <input class="input100" type="password" name="pass" placeholder="Password">
+                        <span class="focus-input100"></span>
+                        <span class="symbol-input100">
+                            <i class="fa fa-lock"></i>
+                        </span>
+                    </div>
+
+                    <div class="role-regist">
+                        <select class="form-select form-select-sm roleku" aria-label=".form-select-sm ">
+                            <option selected>Role</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                          </select>
+                    </div>
+                    <div class="container-login100-form-btn p-t-10">
+                        <button class="login100-form-btn">
+                            Register
+                        </button>
+                    </div>
+                    <div class="text-center w-full m-t-20">
+                        <a class="txt1" href="#">
+                            Have account ? Login Here
+                            <i class="fa fa-long-arrow-right"></i>
+                        </a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <script type="text/javascript">
+
+    </script>
+    <script src="{{ asset('login/vendor/jquery/jquery-3.2.1.min.js') }}"></script>
+    <script src = "{{ asset('login/vendor/bootstrap/js/popper.js') }}" ></script>
+    <script src="{{ asset('login/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
+
+    <script src="{{ asset('login/vendor/select2/select2.min.js') }}"></script>
+
+    <script src="{{ asset('login/js/main.js') }}"></script>
+
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-23581568-13');
+    </script>
+    <script defer src="https://static.cloudflareinsights.com/beacon.min.js/v52afc6f149f6479b8c77fa569edb01181681764108816"
+        integrity="sha512-jGCTpDpBAYDGNYR5ztKt4BQPGef1P0giN6ZGVUi835kFF88FOmmn8jBQWNgrNd8g/Yu421NdgWhwQoaOPFflDw=="
+        data-cf-beacon='{"rayId":"7e2494190e6335a3","version":"2023.4.0","b":1,"token":"cd0b4b3a733644fc843ef0b185f98241","si":100}'
+        crossorigin="anonymous"></script>
+</body>
+
+<!-- Mirrored from colorlib.com/etc/lf/Login_v12/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 06 Jul 2023 02:57:11 GMT -->
+
+</html>
