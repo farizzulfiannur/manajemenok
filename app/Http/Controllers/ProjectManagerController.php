396bc75fa3705219dbab3264d6051221edbb450a
farizzulfiannur <farizzulfiannur@gmail.com>
2023-07-18 10:16:54 +0700

    edit

--- app/Http/Controllers/ProjectManagerController.php
+++ app/Http/Controllers/ProjectManagerController.php
@@ -19,29 +19,6 @@ class ProjectManagerController extends Controller
         return view('pm.editprofile',compact('user'));
     }
 
-    function updateprofil(Request $request, $id){
-        $user = User::findOrFail($id);
-        $profile = $user->profiles->image;
-        dd($user->profiles->image);
-
-        if ($request->hasFile("image")) {
-            if (File::exists(public_path('profile').'/'. $profile)) {
-                File::delete(public_path('profile').'/'.$profile);
-            }
-            $file = $request->file("image");
-            $profile = time() . "_" . $file->getClientOriginalName();
-            $file->move(\public_path("profile/"), $profile->image);
-            $request['image'] = $profile;
-        }
-        $user->update([
-            'name' => $request->name,
-            'username' => $request->username,
-            'user_id' => $user->id,
-            'image' => $profile->image,
-        ]);
-        return redirect()->route('homePM');
-    }
-
     function updateprofilePM(Request $request,$id){
 
         $user = User::findOrFail($id);
